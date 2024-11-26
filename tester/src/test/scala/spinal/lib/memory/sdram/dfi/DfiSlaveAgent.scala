9ba410ad996cd2e902f1233e886753a0f3bdd0c5
Liyaohong <864366810@qq.com>
2024-11-26 21:02:07 +0800

    use BmbMasterAgent.

---
+++ tester/src/test/scala/spinal/lib/memory/sdram/dfi/DfiSlaveAgent.scala
@@ -0,0 +1,12 @@
+package spinal.lib.memory.sdram.dfi
+import spinal.core._
+import spinal.core.sim._
+import spinal.lib.Stream
+import spinal.lib.sim.{SimData, StreamDriver, StreamMonitor, StreamReadyRandomizer}
+class DfiWriteSlaveAgent(){
+
+}
+
+class DfiReadSlaveAgent(){
+
+}
